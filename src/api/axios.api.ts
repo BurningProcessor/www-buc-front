748728f8f1c260a18ece077ed4812892957c793e
BurningProcessor <>
2023-10-17 13:58:27 +0000

    config project
    FossilOrigin-Name: 2c71f11cb954e621960cfa09732d3fe354bb22b228f5a66a08d2605119f08929

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToketFromLocalStorage } from "../helpers/localstorage.helper";
 
 export const instance = axios.create({
-	baseURL: 'http://localhost:3000/api',
+	baseURL: 'http://192.168.0.2:3000/api',
 	headers: {
 		Authorization: 'Bearer ' + getToketFromLocalStorage()
 	}
